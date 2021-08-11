2cab3465476ac2ffcb9647b121ddf72393482fac
sanjeev-thapa <stedwardson@gmail.com>
2021-08-12 04:02:10 +0545

    Make Content of Dashboard Responsive

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
 @section('title') Dashboard @endsection
 
 @section('content')
-<div class="w-25 bg-white p-4 border mx-auto mt-4 rounded">
+<div class="w-25-lg bg-white p-4 border mx-auto mt-4 rounded">
     <p class="m-0">Welcome {{ auth()->user()->name }}</p>
 </div>
 @endsection
