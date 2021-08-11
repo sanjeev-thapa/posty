76dc537e5958d37e94dc0790720119bb8ffb4d1a
sanjeev-thapa <stedwardson@gmail.com>
2021-08-12 04:08:43 +0545

    Allow Only Authenticated User to Post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,17 +4,23 @@
 
 @section('content')
 <div class="w-75-lg bg-white pt-4 px-4 pb-2 border mx-auto mt-4 rounded">
+    @auth
     <form action="{{ route('posts.store') }}" method="post">
         @csrf
-
         <div class="form-group">
             <textarea class="form-control bg-light border-2 focus-primary py-2 @error('body') is-invalid @enderror"
                 placeholder="Post Something..." rows="5" name="body">{{ old('body') }}</textarea>
             @error('body') <p class="text-danger m-0 font-90"> {{ $message }} </p> @enderror
         </div>
-
         <button class="btn btn-primary">Post</button>
     </form>
+    @endauth
+
+    @guest
+    <div class="alert alert-danger"><a class="font-weight-bold text-dark" href="{{ route('login') }}">Login</a> to
+        Post!
+    </div>
+    @endguest
 
     <div class="mt-4 mb-3">
         @foreach ($posts as $post)
