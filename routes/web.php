1e83fe67b2a4395335cbcc2941039673754225ce
sanjeev-thapa <stedwardson@gmail.com>
2021-08-13 09:55:51 +0545

    Refactor Web Route

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,7 @@ Route::view('/', 'home');
 Route::view('dashboard', 'dashboard')->middleware('auth')->name('dashboard');
 
 // Post
-Route::get('posts', [PostController::class, 'index'])->name('posts.index');
-Route::post('posts', [PostController::class, 'store'])->name('posts.store');
-Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
-Route::delete('posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+Route::resource('posts', PostController::class)->only(['index', 'store', 'show', 'destroy']);
 
 // Like
 Route::post('likes/{post}', [LikeController::class, 'store'])->name('likes.store');
