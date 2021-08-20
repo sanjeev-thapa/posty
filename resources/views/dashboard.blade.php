f32cb05a4baa37fd1f98ec83f889c3464b49d054
sanjeev-thapa <stedwardson@gmail.com>
2021-08-20 08:41:59 +0545

    Add Links to Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,6 +4,13 @@
 
 @section('content')
 <div class="w-25-lg bg-white p-4 border mx-auto mt-4 rounded">
-    <p class="m-0">Welcome {{ Str::title(auth()->user()->name) }}</p>
+    <p>Welcome {{ Str::title(auth()->user()->name) }}</p>
+
+    <a class="btn btn-primary" href="{{ route('profiles.show', auth()->user()) }}">View Profile</a>
+    <a class="btn btn-primary" href="{{ route('posts.index') }}">View Posts</a>
+    <form action="{{ route('logout') }}" class="d-inline" method="post">
+        @csrf
+        <button class="btn btn-red mt-lg-2">Logout</button>
+    </form>
 </div>
 @endsection
