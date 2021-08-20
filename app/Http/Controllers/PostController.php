841679f779062ccfa673932b2aa44d7514f4dcb5
sanjeev-thapa <stedwardson@gmail.com>
2021-08-20 10:01:26 +0545

    Add Auth Middleware for Store on PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,10 @@ use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth')->only('store');
+    }
+
     /**
      * Display a listing of the resource.
      *
